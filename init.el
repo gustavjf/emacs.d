2586ea0d5658f1ace63e194af3becb7fcbd378c8
Gustavo Jimenez <gustavojimenez.folta@gmail.com>
2015-09-15 21:47:27 +0200

    Add subdirs of `~/.emacs.d/lisp` to load path
    This is to better organize my customizations in its own folder.

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(let ((default-directory "~/.emacs.d/lisp"))
+  (normal-top-level-add-subdirs-to-load-path))
 (require 'init-benchmarking) ;; Measure startup time
 
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
