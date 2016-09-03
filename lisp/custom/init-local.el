55854719cbc6f0d9b2d932e8ec872e12ed75e395
Gustavo E. Jimenez Folta <gustavo.jimenez@kontist.com>
2016-09-03 17:09:03 +0200

    Add reconfiguration for keyboard macros from Balaji S. Srinivasan

---
+++ lisp/custom/init-local.el
@@ -0,0 +1,10 @@
+;; ------------------------------
+;; -- Gustavo's custom configs --
+;; ------------------------------
+
+;;; Code:
+
+(require 'init-balaji-kmacros)
+
+(provide 'init-local)
+;;; init-local.el ends here
