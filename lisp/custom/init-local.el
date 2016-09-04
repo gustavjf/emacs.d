e727606ffd09154c377a52d332ea713367b1a590
Gustavo E. Jimenez Folta <gustavo.jimenez@kontist.com>
2016-09-04 16:04:36 +0200

    Extend JavaScript configuration
    Sets up:
    1. eslint with flycheck
    2. web-mode for .jsx files
    3. tern-mode and company-tern backend for JavaScript autocompletion

--- lisp/custom/init-local.el
+++ lisp/custom/init-local.el
@@ -7,6 +7,8 @@
 (require 'init-balaji-kmacros)
 (require 'init-git-gutter)
 (require 'init-golang)
+(require 'init-javascript-extended)
+(require 'init-tern)
 
 (provide 'init-local)
 ;;; init-local.el ends here
