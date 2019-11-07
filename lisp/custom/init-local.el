f8e5012c6dd0cd184318b82e9abc2f73e567be01
Gustavo E. Jimenez Folta <gustavojimenez.folta@gmail.com>
2019-11-07 17:18:13 +0100

    Remove things I wasn't using anymore
    Some of these customizations are already in the lisp directory.

--- lisp/custom/init-local.el
+++ lisp/custom/init-local.el
@@ -4,13 +4,8 @@
 
 ;;; Code:
 
-(require 'init-balaji-kmacros)
-(require 'init-flycheck-extended)
 (require 'init-git-gutter)
-(require 'init-golang)
-(require 'init-javascript-extended)
 (require 'init-tern)
-(require 'init-rust)
 (require 'init-undo-tree)
 
 (provide 'init-local)
